533509eadf21d92d3867f50c252b43dd96a03ce2
Marc Pawlowsky <marcpawl@gmail.com>
2023-09-21 18:11:01 -0400

    Render invastion rating

--- website/1page_elm/json2elm.py
+++ website/1page_elm/json2elm.py
@@ -1,9 +1,155 @@
 import json
 import pathlib
 import sys
+from typing import List
 
 
 ARMYLISTS_DIRECTORY = pathlib.Path("../../fake_meshwesh/armyLists")
+
+def make_safe_string(udata) :
+    """Remove special characters."""
+    if udata is None:
+      return None
+    asciidata:str = udata.encode("ascii", "ignore").decode("ascii")
+    asciidata = asciidata.replace('"', "")
+    return asciidata
+
+def quote(x: str) -> str:
+    return '"' + x + '"'
+
+
+def writeStringList(aList: List[str], elm, indent: int):
+    safe_strings = map(make_safe_string, aList)
+    quoted = list(map(quote, safe_strings))
+    writeList(quoted, elm, indent)
+
+
+def writeList(aList: List, elm, indent: int):
+    indent_str = " " * indent 
+    elm.write(indent_str + '[\n')
+    if len(aList) > 0:
+      elm.write(indent_str)
+      elm.write("  ")
+      elm.write(('\n' + indent_str + ', ').join(aList))
+      elm.write('\n')
+    elm.write(indent_str + ']')
+
+
+def toRating(rating) -> str:
+  result =  "InvasionRating " 
+  result = result + quote(rating['_id'])
+  result = result + ' ' + str(rating['value'])
+  result = result + " "
+  if 'note' in rating and rating['note'] is not None:
+    result = result + "(Just " + quote(rating['note']) + ")"
+  else:
+      result = result + 'Nothing' 
+  return result
+
+
+def writeRatings(elm, type: str, army_id: str, ratings):
+  ratings_string: List[str] = list(map(toRating, ratings))
+  variable = type[0].lower() + type[1:] + "s" + "_" + army_id
+  elm.write(variable)
+  elm.write(": List " + type + "\n")
+  elm.write(variable)
+  elm.write(" =\n")
+  writeList(ratings_string, elm, 8)
+  elm.write("\n\n")
+
+
+def writeInvasionRatings(elm, army_id, invasion_ratings):
+  writeRatings(elm, "InvasionRating", army_id, invasion_ratings)
+
+
+def writeManeuverRatings(elm, army_id, ratings):
+  writeRatings(elm, "ManeuverRating", army_id, ratings)
+
+
+def writeArmy(elm, army):
+      army_id = army['id']
+
+      invasion_ratings = army['invasionRatings']
+      writeInvasionRatings(elm, army_id, invasion_ratings)
+
+      maneuver_ratings = army['maneuverRatings']
+      writeManeuverRatings(elm, army_id, maneuver_ratings)
+
+      elm.write(f"army_{army_id}: Army\n")
+      elm.write(f"army_{army_id} =\n")
+      elm.write("  {\n")
+      elm.write(f"""    id = "{army_id}"\n""")
+
+      elm.write("    , keywords =\n")
+      writeStringList(army['keywords'], elm, 8)
+      elm.write("\n")
+
+      elm.write(f"    , listStartDate = {army['derivedData']['listStartDate']}\n")
+      elm.write(f"    , listEndDate = {army['derivedData']['listEndDate']}\n")
+      elm.write(f"""    , extendedName = "{army['derivedData']['extendedName']}"\n""")
+      elm.write(f"    , sortId = {army['sortId']}\n")
+      elm.write(f"""    , sublistId = "{army['sublistId']}"\n""")
+      elm.write(f"""    , name = "{army['name']}"\n""")
+      elm.write(f"""    , invasionRatings = invasionRatings_{army_id}\n""")
+      elm.write(f"""    , maneuverRatings = maneuverRatings_{army_id}\n""")
+      elm.write("  }\n\n")
+
+
+def armies():
+  all_armies = dict()
+  for file in ARMYLISTS_DIRECTORY.glob("*.army.json"):
+    army_id = pathlib.Path(file.stem).stem
+    with file.open() as army:
+      army_data = json.load(army)
+      all_armies[army_id] = army_data
+  with open("src/Armies.elm", "w") as elm:
+    elm.write("module Armies exposing (..)\n")
+    elm.write("""
+              
+type alias InvasionRating =
+  {
+    id: String
+  , value: Int
+  , note: Maybe String
+  }
+              
+type alias ManeuverRating =
+  {
+    id: String
+  , value: Int
+  , note: Maybe String
+  }
+              
+type alias Army =
+  { 
+    id : String
+  , keywords: List String
+  , listStartDate: Int
+  , listEndDate: Int
+  , extendedName: String
+  , sortId: Float
+  , sublistId: String
+  , name: String   
+  , invasionRatings: List InvasionRating
+  , maneuverRatings: List ManeuverRating       
+  }
+              
+""")
+    elm.write("\n")
+
+    def by_name(id1: str) -> str:
+      return all_armies[id1]['name']
+   
+    sorted_id = list(all_armies.keys())
+    sorted_id.sort(key=by_name)
+
+    for army_id in sorted_id:
+      army = all_armies[army_id]
+      writeArmy(elm, army)
+
+    armies_names = list(map(lambda x: "army_" + x, sorted_id))
+    elm.write("all_armies = ")
+    writeList(armies_names, elm, 2)
 
 
 def themes():
@@ -16,12 +162,16 @@ def themes():
         if theme_description['id'] not in all_themes:
           all_themes[theme_description['id']] = { 'name': theme_description['name'], 'armies':[]}
         all_themes[theme_description['id']]['armies'].append(army_id)
+
   with open("src/Themes.elm", "w") as elm:
-    elm.write("module Themes exposing (..)\n")
-    elm.write("""
+    elm.write("""module Themes exposing (..)
+
+import Armies exposing (..)
+              
 type alias Theme =
   { id : String
   , name : String
+  , armies: List Armies.Army
   }
               
 """)
@@ -40,7 +190,10 @@ type alias Theme =
       elm.write("  {\n")
       elm.write(f"""    id = "{theme_id}"\n""")
       elm.write(f"""  , name = "{theme_description['name']}"\n""")
-      # TODO armies
+
+      elm.write("  , armies =\n")
+      armies_names = map(lambda x: "army_" + x, theme_description['armies'])
+      writeList(list(armies_names), elm, 8)
       elm.write("  }\n\n")
 
     elm.write("themes =\n")
@@ -53,4 +206,6 @@ type alias Theme =
 
 
 
+
+armies()
 themes()
--- website/1page_elm/src/Main.elm
+++ website/1page_elm/src/Main.elm
@@ -1,115 +1,355 @@
-module Main exposing(..)
+module Main exposing (..)
 
+import Armies exposing (..)
 import Browser
+import Css exposing (bold, em, fontWeight, padding, px)
 import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
-import Http
-import Platform.Cmd as Cmd
 import Html.Attributes exposing (list)
-import Html
+import Html.Events exposing (onClick)
+import Html.Styled exposing (styled)
+import Html.Styled.Attributes
+import Http
 import Json.Decode as Decode exposing (Decoder)
 import List
-
+import Platform.Cmd as Cmd
 import Themes
-import Css exposing (padding)
-import Css exposing (fontWeight)
-import Css exposing (bold)
-import Css exposing (px)
-import Html.Styled exposing (styled)
-import Css exposing (em)
+
+
+
 -- MODEL
 
-type alias Model =  PageStatus
+
+type alias Model =
+    PageStatus
+
 
 type PageStatus
-  = 
-   WaitingForArmy
-   | Loading String
-  | Loaded String
-  | Error
+    = WaitingForArmy
+    | Loading String
+    | Loaded String
+    | Error
 
 
 init : Model
-init = WaitingForArmy
+init =
+    WaitingForArmy
+
+
 
 -- UPDATE
 
-type Msg = 
-      GetArmy
+
+type Msg
+    = GetArmy
     | DataReceived (Result Http.Error String)
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    GetArmy -> (model, getArmy model)
+    case msg of
+        GetArmy ->
+            ( model, getArmy model )
+
+        DataReceived (Err _) ->
+            ( Error, Cmd.none )
+
+        DataReceived (Ok data) ->
+            ( Loaded data, Cmd.none )
 
 
-    DataReceived (Err _) -> (Error, Cmd.none)
-
-    DataReceived (Ok data) -> 
-        (Loaded data, Cmd.none)
 
 -- VIEW
 
-view model =
-  Html.Styled.toUnstyled (
-  Html.Styled.div []
-    [ 
-    --   button [ onClick GetArmy ] [ text "Start" ],
-    --   button [ onClick GetArmy ] [ text "-" ]
-    -- , div []
-     Html.Styled.text (
-      status model
-      )
-    ,  thematicCategories 
-    ,  thematicCategiesContent
-    --   (text (
-    --   "todo"
-    --   -- String.fromInt model
-    --   ) )
-    ]
-  )
 
-status: Model -> String
+view model =
+    Html.Styled.toUnstyled
+        (Html.Styled.div []
+            [ --   button [ onClick GetArmy ] [ text "Start" ],
+              --   button [ onClick GetArmy ] [ text "-" ]
+              -- , div []
+              Html.Styled.text
+                (status model)
+            , thematicCategoriesTableOfContents
+            , thematicCategiesContent
+            , armiesTableOfContents
+            , armiesDetailed
+
+            --   (text (
+            --   "todo"
+            --   -- String.fromInt model
+            --   ) )
+            ]
+        )
+
+
+status : Model -> String
 status model =
-  case model of 
-    WaitingForArmy -> "Starting"
-    Loading url ->
-      "loading " ++ url
-    Loaded data ->
-      "Loaded " ++ data
-    Error -> "Error"
+    case model of
+        WaitingForArmy ->
+            "Starting"
+
+        Loading url ->
+            "loading " ++ url
+
+        Loaded data ->
+            "Loaded " ++ data
+
+        Error ->
+            "Error"
+
+
+
+-- Value of ID in an element, use for in document HREF
+
+
+anchor_tag : String -> String -> String
+anchor_tag typename id =
+    String.concat [ typename, id ]
+
+
+theme_tag : Themes.Theme -> String
+theme_tag theme =
+    anchor_tag "theme_" theme.id
+
+
+theme_href : Themes.Theme -> Html.Styled.Attribute msg
+theme_href theme =
+    Html.Styled.Attributes.href (String.concat [ "#", theme_tag theme ])
+
+
+army_tag : Armies.Army -> String
+army_tag army =
+    anchor_tag "army_" army.id
+
+
+army_href : Armies.Army -> Html.Styled.Attribute msg
+army_href theme =
+    Html.Styled.Attributes.href (String.concat [ "#", army_tag theme ])
+
 
 thematicCategory : Themes.Theme -> Html.Styled.Html msg
 thematicCategory theme =
-  styledContentLink [] [Html.Styled.text theme.name]
+    styledContentLink
+        []
+        [ Html.Styled.a
+            [ theme_href theme
+            ]
+            [ Html.Styled.text theme.name
+            ]
+        ]
 
-thematicCategories : Html.Styled.Html msg
-thematicCategories =
-  Html.Styled.div []
-    [ 
-         styledContentHeader [] [(Html.Styled.text "Thematic Categories")]
-      ,  Html.Styled.text """Thematic categories are a way of grouping army lists that fit a
+
+
+-- Page that contains the list of all the categories
+
+
+thematicCategoriesTableOfContents : Html.Styled.Html msg
+thematicCategoriesTableOfContents =
+    styledMain []
+        [ styledContentHeader [] [ Html.Styled.text "Thematic Categories" ]
+        , Html.Styled.text """Thematic categories are a way of grouping army lists that fit a
            common period and broad geographic region. Many army lists belong to
           more than one thematic category."""
-      , Html.Styled.div [] (List.map thematicCategory Themes.themes)
-    ]
+        , Html.Styled.div [] (List.map thematicCategory Themes.themes)
+        ]
+
+
+armyNameList : List Army -> Html.Styled.Html msg
+armyNameList armies =
+    Html.Styled.div
+        []
+        (List.map
+            (\army ->
+                styledThematicCategoryContentArmy
+                    []
+                    [ Html.Styled.a
+                        [ army_href army
+                        ]
+                        [ Html.Styled.text army.extendedName
+                        ]
+                    ]
+            )
+            armies
+        )
+
+
 
 -- Contents of a theme
+
+
 thematicCategoryContent : Themes.Theme -> Html.Styled.Html msg
 thematicCategoryContent theme =
-  Html.Styled.div
-    []
-    [
-      Html.Styled.text theme.name
-    ]
+    styledMain []
+        [ Html.Styled.h1
+            [ Html.Styled.Attributes.id (theme_tag theme)
+            ]
+            [ Html.Styled.text theme.name
+            ]
+        , Html.Styled.h2 [] [ Html.Styled.text "Army Lists" ]
+        , armyNameList theme.armies
+        ]
+
+
 
 -- Each theme with the contents of the theme
-thematicCategiesContent :  Html.Styled.Html msg
+-- Each theme is is own "page"
+
+
+thematicCategiesContent : Html.Styled.Html msg
 thematicCategiesContent =
-  Html.Styled.div
+    Html.Styled.div
+        []
+        (List.map thematicCategoryContent Themes.themes)
+
+
+
+-- "Page" containing the list of all the armies
+
+
+armiesTableOfContents : Html.Styled.Html msg
+armiesTableOfContents =
+    styledMain
+        []
+        [ Html.Styled.h1
+            []
+            [ Html.Styled.text "List of all armies" ]
+        , Html.Styled.div
+            []
+            [ armyNameList Armies.all_armies
+            ]
+        ]
+
+
+
+-- Section containing the "Pages" for each army
+
+
+armiesDetailed : Html.Styled.Html msg
+armiesDetailed =
+    Html.Styled.div
+        []
+        (List.map (\army -> armyDetail army) Armies.all_armies)
+
+
+armyDetail : Armies.Army -> Html.Styled.Html msg
+armyDetail army =
+    styledMain
+        [ Html.Styled.Attributes.id (army_tag army)
+        ]
+        [ armyListAndDateRange army
+        , ratingsEtcRendered army
+        ]
+
+
+ratingsEtcRendered : Armies.Army -> Html.Styled.Html msg
+ratingsEtcRendered army =
+    Html.Styled.div
+        []
+        [ invasionRatingRendered army
+        , maneuverRatingsRendered army
+        ]
+
+noteRendered: Maybe String -> Html.Styled.Html msg
+noteRendered note =
+  Html.Styled.td
     []
-    (List.map thematicCategoryContent Themes.themes)
+    (case note of
+      Just x -> [ (Html.Styled.text x) ]
+      Nothing -> []
+    )
+
+ratingValue: Armies.InvasionRating  -> Int
+ratingValue rating =
+  rating.value
+
+ratingsRowRendered : Armies.InvasionRating -> Html.Styled.Html msg
+ratingsRowRendered rating =
+  Html.Styled.tr 
+    [] 
+    [
+      Html.Styled.td 
+        [] 
+        [
+    Html.Styled.text (String.fromInt (ratingValue rating))
+    ]
+    , 
+    (noteRendered rating.note)
+    ]
+
+
+invasionRatingRendered : Armies.Army -> Html.Styled.Html msg
+invasionRatingRendered army =
+    Html.Styled.div
+        []
+        [ styledContentSubHeader
+            []
+            [ Html.Styled.text "Invasion Rating"
+            ]
+        , Html.Styled.table
+            []
+            [ Html.Styled.tbody [] 
+              (List.map ratingsRowRendered army.invasionRatings)
+            ]
+        ]
+
+maneuverRatingsRendered: Armies.Army -> Html.Styled.Html msg
+maneuverRatingsRendered _ =
+  Html.Styled.div [] []
+
+
+armyListAndDateRange : Armies.Army -> Html.Styled.Html msg
+armyListAndDateRange army =
+    Html.Styled.div
+        []
+        [ Html.Styled.h1
+            []
+            [ Html.Styled.span
+                []
+                [ Html.Styled.text army.name
+                ]
+            ]
+        , renderedDateRange army.listStartDate army.listEndDate
+        ]
+
+
+renderedDateRange : Int -> Int -> Html.Styled.Html msg
+renderedDateRange startDate endDate =
+    Html.Styled.text (formattedDateRange startDate endDate)
+
+
+era : Int -> String
+era date =
+    if date < 0 then
+        "B.C"
+
+    else
+        "A.D."
+
+
+formattedDate : Int -> String
+formattedDate date =
+    let
+        date_string =
+            String.fromInt (abs date)
+
+        era_string =
+            era date
+    in
+    String.concat [ date_string, " ", era_string ]
+
+
+formattedDateRange : Int -> Int -> String
+formattedDateRange startDate endDate =
+    if startDate == endDate then
+        formattedDate startDate
+
+    else
+        String.concat
+            [ formattedDate startDate
+            , " to "
+            , formattedDate endDate
+            ]
+
 
 getArmy : Model -> Cmd Msg
 getArmy _ =
@@ -118,36 +358,113 @@ getArmy _ =
         , expect = Http.expectString DataReceived
         }
 
+
 styledContentHeader : List (Html.Styled.Attribute msg) -> List (Html.Styled.Html msg) -> Html.Styled.Html msg
 styledContentHeader =
     styled Html.Styled.div
-        [ 
-            Css.fontFamilies ["Helvetica Neue", "Helvetica", "Arial", "sans-serif"]
-          , Css.lineHeight  (Css.num 1.42857143)
-          , Css.color (Css.rgb 0x33 0x33 0x33)
-          , Css.marginTop (px 16)
-          , Css.marginBottom (px 20)
-          , Css.fontSize (em 2)
+        [ Css.fontFamilies [ "Helvetica Neue", "Helvetica", "Arial", "sans-serif" ]
+        , Css.lineHeight (Css.num 1.42857143)
+        , Css.color (Css.rgb 0x33 0x33 0x33)
+        , Css.marginTop (px 16)
+        , Css.marginBottom (px 20)
+        , Css.fontSize (em 2)
         ]
-    -- TODO
-    -- -webkit-text-size-adjust: 100%;
-    -- -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+
+
+
+-- TODO
+-- -webkit-text-size-adjust: 100%;
+-- -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+
+
+styledContentSubHeader : List (Html.Styled.Attribute msg) -> List (Html.Styled.Html msg) -> Html.Styled.Html msg
+styledContentSubHeader =
+    styled Html.Styled.div
+        [ Css.fontFamilies [ "Helvetica Neue", "Helvetica", "Arial", "sans-serif" ]
+        , Css.lineHeight (Css.num 1.42857143)
+        , Css.color (Css.rgb 0x33 0x33 0x33)
+        , Css.boxSizing Css.borderBox
+        , Css.fontSize (em 1.2)
+        , Css.marginTop (px 10)
+        , Css.marginBottom (px 20)
+        , Css.fontWeight Css.bold
+        ]
+
+
+
+-- TODO
+--  -webkit-text-size-adjust: 100%;
+-- -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+
 
 styledContentLink : List (Html.Styled.Attribute msg) -> List (Html.Styled.Html msg) -> Html.Styled.Html msg
 styledContentLink =
     styled Html.Styled.div
-      [
-            Css.fontFamilies ["Helvetica Neue", "Helvetica", "Arial", "sans-serif"]
-          , Css.lineHeight  (Css.num 1.42857143)
-          , Css.fontSize (em 1.5)
-          , Css.boxSizing Css.borderBox
-          , Css.backgroundColor Css.transparent
-          , Css.color (Css.rgb 0x33 0x7a 0xb7)
-          , Css.textDecoration Css.none
-      ]
+        [ Css.fontFamilies [ "Helvetica Neue", "Helvetica", "Arial", "sans-serif" ]
+        , Css.lineHeight (Css.num 1.42857143)
+        , Css.fontSize (em 1.5)
+        , Css.boxSizing Css.borderBox
+        , Css.backgroundColor Css.transparent
+        , Css.color (Css.rgb 0x33 0x7A 0xB7)
+        , Css.textDecoration Css.none
+        ]
 
 
-  -- MAIN
+styledThematicCategoryContentArmies : List (Html.Styled.Attribute msg) -> List (Html.Styled.Html msg) -> Html.Styled.Html msg
+styledThematicCategoryContentArmies =
+    styled Html.Styled.div
+        [ Css.fontFamily Css.inherit
+        , Css.fontWeight (Css.int 400)
+        , Css.lineHeight (Css.num 1)
+        , Css.boxSizing Css.borderBox
+        , Css.backgroundColor Css.transparent
+        , Css.color (Css.rgb 0x33 0x7A 0xB7)
+        , Css.textDecoration Css.none
+        ]
+
+
+
+-- TODO
+-- -webkit-text-size-adjust: 100%;
+-- -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+-- font-size: 65%;
+
+
+styledThematicCategoryContentArmy : List (Html.Styled.Attribute msg) -> List (Html.Styled.Html msg) -> Html.Styled.Html msg
+styledThematicCategoryContentArmy =
+    styled Html.Styled.div
+        [ Css.fontFamily Css.inherit
+        , Css.fontWeight (Css.int 400)
+        , Css.lineHeight (Css.num 1.5)
+        , Css.boxSizing Css.borderBox
+        , Css.backgroundColor Css.transparent
+        , Css.color (Css.rgb 0x33 0x7A 0xB7)
+        , Css.textDecoration Css.none
+        , Css.fontSize (Css.pct 65)
+        ]
+
+
+styledMain : List (Html.Styled.Attribute msg) -> List (Html.Styled.Html msg) -> Html.Styled.Html msg
+styledMain =
+    styled Html.Styled.div
+        [ Css.fontFamilies [ "Helvetica Neue", "Helvetica", "Arial", "sans-serif" ]
+        , Css.lineHeight (Css.num 1.42857143)
+        , Css.fontSize (Css.px 14)
+        , Css.boxSizing Css.borderBox
+        , Css.color (Css.rgb 0x33 0x33 0x33)
+        , Css.textDecoration Css.none
+        , Css.marginTop (Css.em 3)
+        , Css.borderBottom3 (Css.px 3) Css.solid (Css.rgb 120 120 120)
+        , Css.paddingBottom (Css.em 1)
+        , Css.paddingLeft (Css.em 1)
+        ]
+
+
+
+-- TODO
+-- -webkit-text-size-adjust: 100%;
+-- -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+-- MAIN
 
 
 main : Program () Model Msg
@@ -158,5 +475,3 @@ main =
         , update = update
         , subscriptions = \_ -> Sub.none
         }
-
-
