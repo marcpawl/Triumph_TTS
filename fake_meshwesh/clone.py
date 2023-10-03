c3a98ef325b684a1fa8d6f252099d81f81a47e8d
Marc Pawlowsky <marcpawl@gmail.com>
2023-10-03 15:20:16 -0400

    Retrieve the list of enemies of the armies from Meshwesh

--- fake_meshwesh/clone.py
+++ fake_meshwesh/clone.py
@@ -2,6 +2,7 @@ import json
 import os
 import sys
 import subprocess
+import time
 
 #https://meshwesh.wgcwar.com/api/v1/
 
@@ -25,12 +26,28 @@ def retrieve_theme(id) :
     cmd = [ "curl", "-o", dest_file, src_url]
     subprocess.check_call(cmd)
 
+#   https://meshwesh.wgcwar.com/api/v1/armyLists/{army-list-id}/enemyArmy
+def retrieve_enemies(id) :
+    dest_file = "armyLists/" + id  + ".enemyArmy.json"
+    src_url = "https://meshwesh.wgcwar.com/api/v1/armyLists/" + id + "/enemyArmyLists"
+    cmd = [ "curl", "-o", dest_file, src_url]
+    subprocess.check_call(cmd)
 
-with open("armyLists/summary.json", "r") as summary_file:
-  summary_text = summary_file.read()
-summary = json.loads(summary_text)
-for army_entry in summary :
-    print(army_entry['id'])
-    retrieve_army(army_entry['id'])
-    retrieve_allyOptions(army_entry['id'])
-    retrieve_theme(army_entry['id'])
+
+def create_timestamp():
+    """Write the time the data was retrieved."""
+    with open("armyLists/timestamp.json", "w") as timestamp_file:
+        timestamp_file.write("%d\n" % (int(time.time())))
+
+
+if __name__ == "__main__":
+    create_timestamp()
+    with open("armyLists/summary.json", "r") as summary_file:
+      summary_text = summary_file.read()
+    summary = json.loads(summary_text)
+    for army_entry in summary :
+        print(army_entry['id'])
+        retrieve_army(army_entry['id'])
+        retrieve_allyOptions(army_entry['id'])
+        retrieve_theme(army_entry['id'])
+        retrieve_enemies(army_entry['id'])
